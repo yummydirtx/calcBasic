3e983a33d3013abc32cbf34c7ec0824b606bc546
Alex Frutkin <frutkinalexander@gmail.com>
2020-07-19 14:56:55 -0400

    now takes command line arguments

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include "random.hpp"
  
 // get base random alias which is auto seeded and has static API and internal state
@@ -6,10 +7,21 @@ using Random = effolkronium::random_static;
 
 int main(int argc, char *argv[]) {
     int maxNumber{1000};
+    int iterations{1000};
     int tries{0};
     long long int guess{0};
     int lowestNumber{100000000};
-    for (int i{0}; i < 1000; i++) {
+    if (argc == 3) {
+        std::stringstream convert1{argv[1]};
+        std::stringstream convert2{argv[2]};
+        int first;
+        convert1 >> first;
+        int second;
+        convert2 >> second;
+        maxNumber = int(first);
+        iterations = int(second);
+    }
+    for (int i{0}; i < iterations; i++) {
         guess = 0;
         tries = 0;
         while (!(guess == maxNumber)) {
